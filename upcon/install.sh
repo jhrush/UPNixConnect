ef7ad87deb35a274c93bfb47906c42999ebc5911
jhrush <jhrush53@gmail.com>
2021-06-21 10:04:55 -0500

    Quick Commit

--- upcon/install.sh
+++ upcon/install.sh
@@ -1,3 +1,4 @@
 sudo cp Union_Pacific.png /usr/share/pixmaps/
-sudo cp upcon /bin/
+sudo cp upcon /bin/ && sudo cp upconExtra /bin/
 sudo cp unionpacific.desktop /usr/share/applications/
+sudo cp unionpacificExtra.desktop /usr/share/applications/
