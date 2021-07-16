930e535d0f72adb574ece810d46fbf9f2752f22b
jhrush21 <jhrush53@gmail.com>
2021-07-16 08:59:09 -0500

    Quick Commit

--- upcon/install.sh
+++ upcon/install.sh
@@ -2,4 +2,4 @@ sudo cp Union_Pacific.png /usr/share/pixmaps/
 sudo cp upcon /bin/
 sudo cp unionpacific.desktop /usr/share/applications/
 sudo cp unionpacificWithSound.desktop /usr/share/applications/
-#sudo cp EVPN.desktop /usr/share/applications/
+#sudo cp webapp-EVPN4021.desktop /usr/share/applications/
