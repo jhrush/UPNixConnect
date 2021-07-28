773e1074f44bb1b3d05f65b8a23cb7e34043514c
jhrush <jhrush53@gmail.com>
2021-07-28 10:31:53 -0500

    Closes the evpn window after the rdp shortcut is launched

--- upcon/install.sh
+++ upcon/install.sh
@@ -2,4 +2,5 @@ sudo cp Union_Pacific.png /usr/share/pixmaps/
 sudo cp upcon /bin/
 sudo cp unionpacific.desktop /usr/share/applications/
 sudo cp unionpacificWithSound.desktop /usr/share/applications/
+sudo cp xfreerdp.desktop /usr/share/applications/
 #sudo cp webapp-EVPN4021.desktop /usr/share/applications/
